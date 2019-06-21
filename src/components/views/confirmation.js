6e44cfa11d454b973df3383569d03aae6322b98e
brtrduon <brianduongt@gmail.com>
2019-06-21 10:47:38 -0700

    confirm page and gallery css

--- src/components/views/confirmation.js
+++ src/components/views/confirmation.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Confirmation = () => {
     return (
-        <div>Confirmation</div>
+        <div className='body'>
+            <p>
+                Your confirmation has been confirmed.
+            </p>
+            <p>
+                <Link to='/'>Click here</Link> to return to the home page.
+            </p>
+        </div>
     )
 }
 
