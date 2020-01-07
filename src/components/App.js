f1e33ef28fc6faaa05329159375e2127a5dd7786
brtrduon <brianduongt@gmail.com>
2020-01-06 17:34:40 -0800

    updated file names

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 
-import Header from './Header';
-import Footer from './Footer';
+import Header from './header';
+import Footer from './footer';
 
 import Home from './Home';
 import About from './About';
